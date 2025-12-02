2b558b9c3134118e6391c86e50812850b505bf0a
Shafait98 <farhan98aiub@gmail.com>
2025-12-03 05:12:19 +0600

    created a leap year checking function

---
+++ leap-year.js
@@ -0,0 +1,21 @@
+function leapYear(year)
+{
+    if(year % 100 !==0 && year % 4 === 0)
+    {
+        return true;
+    }
+    else if(year % 100 === 0 && year % 400 ===0)
+    {
+        return true
+    }
+
+    else
+        {
+            return false;
+        }
+}
+
+const show = leapYear(2012);
+const show2 = leapYear(2013);
+console.log(show);
+console.log(show2);
