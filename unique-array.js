66677ee11b396abae8d006eb97ec6d0bd7f331cc
Shafait98 <farhan98aiub@gmail.com>
2025-12-14 00:42:49 +0600

    scrapping

--- unique-array.js
+++ unique-array.js
@@ -1,17 +1,35 @@
 // You have to trim all duplicate elements from an array
 
-function trimDuplicate(array)
+// function trimDuplicate(array)
+// {
+//     const unique = [];
+//     for(item of array)
+//     {
+//         if(unique.includes(item) === false)
+//         {
+//             unique.push(item);
+//         }
+//     }
+//     return unique;
+// }
+
+// const view = trimDuplicate(['Farhan','Samanta','Farhan','Rakib','Salek','Samanta']);
+// console.log(view);
+
+function scrapArray(array)
 {
-    const unique = [];
-    for(item of array)
+  const unique = [];
+
+  for(item of array)
+  {
+    if(unique.includes(item) === false)
     {
-        if(unique.includes(item) === false)
-        {
-            unique.push(item);
-        }
+        unique.push(item);
     }
-    return unique;
+  }
+
+  return unique;
 }
 
-const view = trimDuplicate(['Farhan','Samanta','Farhan','Rakib','Salek','Samanta']);
-console.log(view);
+const play = scrapArray([2,2,2,5,8,7,7,8,9,45,45,45]);
+console.log(play);
