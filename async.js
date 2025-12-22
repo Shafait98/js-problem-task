eedeb3220fc43186669c57486f18b66e00b02f80
Shafait98 <farhan98aiub@gmail.com>
2025-12-23 01:24:00 +0600

    Exploring Date & time and manupulation

---
+++ async.js
@@ -0,0 +1,4 @@
+setTimeout(() => 
+{
+    console.log("Hello, This is async");
+},2000) ;
---
+++ date-time.js
@@ -0,0 +1,18 @@
+// const now = new Date();
+// console.log(now);
+// console.log(now.getDay());
+
+// console.log(now.getHours());
+
+//
+
+const year = 2025;
+const month = 11;
+const day = 22;
+const hour = 10;
+const minute = 35;
+const second = 0;
+
+const d = new Date(year,month,day,hour,minute,second);
+
+console.log(d.getDate().toString());
