6b30b5faf99983d23e82c9f0fb3717896df71c2a
Shafait98 <farhan98aiub@gmail.com>
2026-01-07 03:19:18 +0600

    reduced the time complexity combining two loops in a one

--- odd-average.js
+++ odd-average.js
@@ -1,32 +1,55 @@
 // Make a function called oddAverage() take a number of array as a parameter, calculate the average of odd number of the array
 
-function oddAverage(number)
-{
-    let oddNum = [];
-    let sum = 0;
-    let avg;
+// function oddAverage(number)
+// {
+//     let oddNum = [];
+//     let sum = 0;
+//     let avg;
  
 
-    for(num of number)
+//     for(num of number)
+//     {
+//       if(num % 2 === 1)
+//       {
+//         oddNum.push(num);
+//       }
+//     }
+// //    console.log(oddNum);
+//     const len = oddNum.length;
+
+//     for(num of oddNum)
+//     {
+//         sum = sum +num;
+//     }
+//     // console.log(sum);
+//    avg = sum / len;
+
+//    console.log("The odd average is: ", avg);
+
+// }
+
+// const show = oddAverage([2,3,4,5,6,7,8,9,11,13,14,15,16]);
+// console.log
+
+//** Optimized version */
+
+function oddAverage(numbers)
+{
+  let sum = 0;
+  let count = 0;
+
+  for(num of numbers )
+  {
+    if(num % 2 === 1)
     {
-      if(num % 2 === 1)
-      {
-        oddNum.push(num);
-      }
+      sum += num;
+      count++;
     }
-//    console.log(oddNum);
-    const len = oddNum.length;
-
-    for(num of oddNum)
-    {
-        sum = sum +num;
-    }
-    // console.log(sum);
-   avg = sum / len;
-
-   console.log("The odd average is: ", avg);
-
+  }
+   console.log(count);
+   console.log(sum);
+  return sum / count;
 }
 
-const show = oddAverage([2,3,4,5,6,7,8,9,11,13,14,15,16]);
-// console.log
+console.log(oddAverage([1,2,3,4,5,6,7,8,9,22,33,53,43,47,657,433,4343,43,45,422]));
+
