2ad41fa32c0a5f04d66af4f385fbda071aeb1b38
Shafait98 <farhan98aiub@gmail.com>
2026-01-12 02:16:25 +0600

    celsius to Fahrenheit convert function created

---
+++ celsiusToFahr.js
@@ -0,0 +1,10 @@
+//** Write a function to convert temperature from Celsius to Fahrenheit. */
+
+function convert(cel)
+{
+    far = (cel * 9/5) + 32;
+    return far
+}
+
+let play = convert(12);
+console.log(play);
