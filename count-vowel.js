257f828f330a0edfe2b8457070cb821f160eb98d
Shafait98 <farhan98aiub@gmail.com>
2026-01-19 17:06:11 +0600

    optimized the countVowel function

--- count-vowel.js
+++ count-vowel.js
@@ -1,13 +1,34 @@
 // Task-3:
 // Write a function to count the number of vowels in a string.
 
+// function countVowel(str)
+// {
+//     let count = 0;
+
+//     for(let c of str)
+//     {
+//         if(c =='a' || c =='e' || c =='i' || c =='o' || c =='u')
+//         {
+//             count++; 
+//         }
+//     }
+
+//     return count;
+// }
+
+// const play = countVowel('farhan');
+// console.log(play);
+
+//optimized version
+
 function countVowel(str)
 {
     let count = 0;
+    const vowel = 'aeiou';
 
-    for(let c of str)
+    for(let char of str.toLowerCase())
     {
-        if(c =='a' || c =='e' || c =='i' || c =='o' || c =='u')
+        if(vowel.includes(char))
         {
             count++;
         }
@@ -16,5 +37,5 @@ function countVowel(str)
     return count;
 }
 
-const play = countVowel('farhan');
+const play = countVowel("Shafait");
 console.log(play);
